c7b15009ad710477eba3e770098f91a33d6142f4
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-01-02 22:38:20 +0630

    Add favicon

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
     <title>Personal Finance Tracker</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="shortcut icon" href="https://www.flaticon.com/free-icon/financial_12266896" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/icon/favicon.png" type="image/png">
 </head>
 <body class="bg-light">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
